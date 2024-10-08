60b26800bd5baa436d5a70fbb50b7b849b705551
Roy Anger <roy@royanger.com>
2024-10-08 17:07:05 -0400

    Updated web app and extension, added background worker to extension

--- apps/chrome-extension/src/background/index.ts
+++ apps/chrome-extension/src/background/index.ts
@@ -1,13 +1,27 @@
-import { getToken } from './get-token'
+import { __unstable__createClerkClient as createClerkClient } from '@clerk/chrome-extension/background';
 
-console.log('BACKGROUND SCRIPT LOADING')
+console.log('Background Script w/ Clerk createClerkClient() demo loaded')
 
-const getAndPrintToken = async () => {
-  const token = await getToken()
-  console.log("clerk token", token)
-  setTimeout(() => {
-    getAndPrintToken()
-  }, 10000)
+const publishableKey = process.env.PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY
+if (!publishableKey) {
+  throw new Error('Please add the PLASMO_PUBLIC_CLERK_PUBLISHABLE_KEY to the .env.development file')
 }
 
-getAndPrintToken()
+async function getToken() {
+  const clerk = await createClerkClient({
+    publishableKey,
+    syncHost: process.env.PLASMO_PUBLIC_CLERK_SYNC_HOST
+  });
+  return await clerk.session?.getToken();
+}
+
+// create a listener to listen for messages from content scripts
+// NOTE: A runtime listener cannot be async.
+//       It must return true, in order to keep the connection open and send a response later.
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log('Handling request for the user\s current token')
+  getToken().then((token) => sendResponse({ token }));
+  return true;
+});
+
+
